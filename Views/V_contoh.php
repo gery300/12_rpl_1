2831a321966d151f97fdabb2e55a0cc0989772f7
gery300 <sentosagery@gmail.com>
2024-12-16 09:17:59 +0700

    membuat tampilan pada v_contoh.php

--- Views/V_contoh.php
+++ Views/V_contoh.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title>
 </head>
 <body>
-    
+    <h1>hello word</h1>
+    <p>dibuat oleh oleh</p>    
 </body>
 </html>
